f27bad2fc5738383822349ac42c82b7510762f1c
Linus Walker <w000alker@gmail.com>
2023-08-04 13:53:58 +0630

    scratchpads supported

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use confy::load;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
 use penrose::{
     builtin::{
         actions::{exit, key_handler, modify_with, send_layout_message, spawn},
@@ -8,12 +11,14 @@ use penrose::{
         bindings::{parse_keybindings_with_xmodmap, KeyEventHandler},
         Config, WindowManager,
     },
+    extensions::hooks::{
+        add_named_scratchpads, manage::FloatingCentered, NamedScratchPad, ToggleNamedScratchPad,
+    },
     map, util,
+    x::query::ClassName,
     x11rb::RustConn,
     Result,
 };
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use tracing_subscriber::{self, prelude::*};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
@@ -21,52 +26,56 @@ struct WMConfig {
     bindings: HashMap<String, String>,
 }
 
-impl WMConfig {
-    fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
-        let mut raw_bindings = map! {
-            map_keys: |k: &str| k.to_string();
+fn raw_key_bindings(
+    toggle_1: ToggleNamedScratchPad,
+    toggle_2: ToggleNamedScratchPad,
+) -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
+    let mut raw_bindings = map! {
+        map_keys: |k: &str| k.to_string();
 
-            "M-j" => modify_with(|cs| cs.focus_down()),
-            "M-k" => modify_with(|cs| cs.focus_up()),
-            "M-S-j" => modify_with(|cs| cs.swap_down()),
-            "M-S-k" => modify_with(|cs| cs.swap_up()),
-            "M-S-q" => modify_with(|cs| cs.kill_focused()),
-            "M-Tab" => modify_with(|cs| cs.toggle_tag()),
-            "M-bracketright" => modify_with(|cs| cs.next_screen()),
-            "M-bracketleft" => modify_with(|cs| cs.previous_screen()),
-            "M-grave" => modify_with(|cs| cs.next_layout()),
-            "M-S-grave" => modify_with(|cs| cs.previous_layout()),
-            "M-S-Up" => send_layout_message(|| IncMain(1)),
-            "M-S-Down" => send_layout_message(|| IncMain(-1)),
-            "M-S-Right" => send_layout_message(|| ExpandMain),
-            "M-S-Left" => send_layout_message(|| ShrinkMain),
-            "M-semicolon" => spawn("dmenu_run"),
-            "M-S-Return" => spawn("alacritty"),
-            "M-A-Escape" => exit(),
-        };
+        "M-j" => modify_with(|cs| cs.focus_down()),
+        "M-k" => modify_with(|cs| cs.focus_up()),
+        "M-S-j" => modify_with(|cs| cs.swap_down()),
+        "M-S-k" => modify_with(|cs| cs.swap_up()),
+        "M-S-q" => modify_with(|cs| cs.kill_focused()),
+        "M-Tab" => modify_with(|cs| cs.toggle_tag()),
+        "M-bracketright" => modify_with(|cs| cs.next_screen()),
+        "M-bracketleft" => modify_with(|cs| cs.previous_screen()),
+        "M-grave" => modify_with(|cs| cs.next_layout()),
+        "M-S-grave" => modify_with(|cs| cs.previous_layout()),
+        "M-S-Up" => send_layout_message(|| IncMain(1)),
+        "M-S-Down" => send_layout_message(|| IncMain(-1)),
+        "M-S-Right" => send_layout_message(|| ExpandMain),
+        "M-S-Left" => send_layout_message(|| ShrinkMain),
+        "M-semicolon" => spawn("dmenu_run"),
+        "M-Return" => spawn("alacritty"),
+        "M-A-Escape" => exit(),
 
-        if let Ok(cfg) = load::<WMConfig>("sswm", Some("config")) {
-            for i in cfg.bindings {
-                let r = i.1.clone();
-                raw_bindings.insert(i.0, key_handler(move |_, _| util::spawn(r.clone())));
-            }
+        "M-slash" => Box::new(toggle_1),
+        "M-p" => Box::new(toggle_2),
+    };
+
+    if let Ok(cfg) = load::<WMConfig>("sswm", Some("config")) {
+        for i in cfg.bindings {
+            let r = i.1.clone();
+            raw_bindings.insert(i.0, key_handler(move |_, _| util::spawn(r.clone())));
         }
-
-        for tag in &["1", "2", "3", "4", "5", "6", "7", "8", "9"] {
-            raw_bindings.extend([
-                (
-                    format!("M-{tag}"),
-                    modify_with(move |client_set| client_set.focus_tag(tag)),
-                ),
-                (
-                    format!("M-S-{tag}"),
-                    modify_with(move |client_set| client_set.move_focused_to_tag(tag)),
-                ),
-            ]);
-        }
-
-        raw_bindings
     }
+
+    for tag in &["1", "2", "3", "4", "5", "6", "7", "8", "9"] {
+        raw_bindings.extend([
+            (
+                format!("M-{tag}"),
+                modify_with(move |client_set| client_set.focus_tag(tag)),
+            ),
+            (
+                format!("M-S-{tag}"),
+                modify_with(move |client_set| client_set.move_focused_to_tag(tag)),
+            ),
+        ]);
+    }
+
+    raw_bindings
 }
 
 fn main() -> Result<()> {
@@ -75,23 +84,28 @@ fn main() -> Result<()> {
         .finish()
         .init();
 
+    let (nsp_1, toggle_1) = NamedScratchPad::new(
+        "terminal",
+        "alacritty",
+        ClassName("Alacritty"),
+        FloatingCentered::new(0.8, 0.8),
+        true,
+    );
+    let (nsp_2, toggle_2) = NamedScratchPad::new(
+        "qt-console",
+        "jupyter-qtconsole",
+        ClassName("jupyter-qtconsole"),
+        FloatingCentered::new(0.8, 0.8),
+        true,
+    );
+
     let conn = RustConn::new()?;
-    let key_bindings = parse_keybindings_with_xmodmap(WMConfig::raw_key_bindings())?;
-    let wm = WindowManager::new(Config::default(), key_bindings, HashMap::new(), conn)?;
+    let key_bindings = parse_keybindings_with_xmodmap(raw_key_bindings(toggle_1, toggle_2))?;
+
+    let wm = add_named_scratchpads(
+        WindowManager::new(Config::default(), key_bindings, HashMap::new(), conn)?,
+        vec![nsp_1, nsp_2],
+    );
 
     wm.run()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn bindings_parse_correctly_with_xmodmap() {
-        let res = parse_keybindings_with_xmodmap(raw_key_bindings());
-
-        if let Err(e) = res {
-            panic!("{e}");
-        }
-    }
 }
